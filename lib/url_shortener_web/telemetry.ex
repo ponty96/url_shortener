5cde23018ae70c08a7d9630dcf880e8ebd2eba17
Ayomide Aregbede <ayo.aregbede@gmail.com>
2021-10-06 20:57:00 +0100

    fix up

--- lib/url_shortener_web/telemetry.ex
+++ lib/url_shortener_web/telemetry.ex
@@ -13,7 +13,7 @@ defmodule UrlShortenerWeb.Telemetry do
       # every 10_000ms. Learn more here: https://hexdocs.pm/telemetry_metrics
       {:telemetry_poller, measurements: periodic_measurements(), period: 10_000},
       # Add reporters as children of your supervision tree.
-      {Telemetry.Metrics.ConsoleReporter, metrics: metrics()}
+      # {Telemetry.Metrics.ConsoleReporter, metrics: metrics()}
     ]
 
     Supervisor.init(children, strategy: :one_for_one)
