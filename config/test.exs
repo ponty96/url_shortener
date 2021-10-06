753ec9184e7beb970eecfdd9a96ea65401f5d2f4
Ayomide Aregbede <ayo.aregbede@gmail.com>
2021-10-06 22:34:42 +0100

    fix?

--- config/test.exs
+++ config/test.exs
@@ -15,8 +15,8 @@ config :url_shortener, UrlShortener.Repo,
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
 config :url_shortener, UrlShortenerWeb.Endpoint,
-  http: [port: 4002],
-  server: false
+  http: [port: 4000],
+  server: true
 
 # Print only warnings and errors during test
 config :logger, level: :warn
